96e14b0b245bccc4f18967d3e1f7b7f9994538c1
SpacePudding <michael.vu9966@gmail.com>
2024-05-25 14:59:44 +0200

    Disable the example endpoint

--- function_app.py
+++ function_app.py
@@ -4,26 +4,26 @@ import main
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="HttpExample")
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+# @app.route(route="HttpExample")
+# def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. Can deez nuts fit in yo mouth.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+#     if name:
+#         return func.HttpResponse(f"Hello, {name}. Can deez nuts fit in yo mouth.")
+#     else:
+#         return func.HttpResponse(
+#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#              status_code=200
+#         )
 
 @app.route(route="timeReport", auth_level=func.AuthLevel.ANONYMOUS)
 def timeReport(req: func.HttpRequest) -> func.HttpResponse:
