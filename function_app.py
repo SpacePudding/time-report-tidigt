09b58f45851bd45ed839752c6729ddd27c136dcb
SpacePudding <michael.vu9966@gmail.com>
2024-07-12 20:06:30 +0200

    Completed core functionality of time reporting

--- function_app.py
+++ function_app.py
@@ -1,9 +1,9 @@
 import azure.functions as func
 import logging
+import workhours
 import os
 import requests
 from datetime import datetime
-
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -11,18 +11,18 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def TimeReport(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # Obtain date in yyyy-mm-dd format
-    current_date = datetime.now().date()
-    formatted_date = current_date.strftime("%Y-%m-%d")
-    work_in_minutes = 480
+    time_report = workhours.obtain_workhours_this_month()
 
-    timeReportConsid(formatted_date, work_in_minutes)
+    timeReportConsid(time_report)
 
     return func.HttpResponse("TimeReport was a success!")
 
 
-def timeReportConsid(formatted_date, work_in_minutes):
+def timeReportConsid(time_report):
 
+    session = requests.session()
+
+    endpoint_current_time = 'https://tidig.consid.net/Time/TimeRangeData'
     endpoint_time_report = 'https://tidig.consid.net/Time/PostTimeForm'
 
     cookies = {
@@ -31,16 +31,48 @@ def timeReportConsid(formatted_date, work_in_minutes):
         '.AspNetCore.CookiesC2': os.environ["CHUNK2"]
     }
 
-    time_report_data = {
-        "dates": [formatted_date], 
-        "articleId": 1, # Normal
-        "amountMinutes": work_in_minutes, 
-        "customerId": 2, # Consid AB
-        "projectId": 3313, # Internt
-        "activity": "Certifiering",
-        "caseNumber": None,
-        "description": "Normal working hours",
-        "employeeChildren": [],
-        "customerManagerName": None}
+    response = session.get(endpoint_current_time, params=obtain_current_time_endpoint_params(), cookies=cookies)
+    current_work_data = response.json()
 
-    requests.post(endpoint_time_report, json=time_report_data, cookies=cookies)
+    for entry in time_report:
+        for timeEntry in current_work_data['timeDateData']:
+            if timeEntry['date'] == entry and timeEntry['reportedMinutesForTimeBank'] == 0:
+
+                time_report_data = {
+                    "dates": [entry], 
+                    "articleId": 1, # Normal
+                    "amountMinutes": time_report[entry], 
+                    "customerId": 2080, # Borgwarner
+                    "caseNumber": None,
+                    "description": "Normal arbetstimmar hos kund",
+                    "employeeChildren": [],
+                    "operatingUnitId": 1152,
+                    "customerManagerName": None,
+                    "workPlaceId": "AtCustomer"}
+                
+                travel_report_data = {
+                    "dates": [entry], 
+                    "articleId": 23, # Normal
+                    "amountMinutes": 60, 
+                    "customerId": 2080, # Borgwarner
+                    "caseNumber": None,
+                    "description": None,
+                    "employeeChildren": [],
+                    "operatingUnitId": 1152,
+                    "customerManagerName": None,
+                    "workPlaceId": "AtCustomer"}
+
+                session.post(endpoint_time_report, json=time_report_data, cookies=cookies)
+                session.post(endpoint_time_report, json=travel_report_data, cookies=cookies)
+
+def obtain_current_time_endpoint_params():
+
+    current_date = datetime.now()
+
+    # Get the first day of the current month
+    first_day_of_month = current_date.replace(day=1).strftime('%Y-%m-%d')
+
+    # Get the current date in yyyy-mm-dd format
+    current_date_str = current_date.strftime('%Y-%m-%d')
+
+    return {'startDate': f'{first_day_of_month}','endDate': f'{current_date_str}'}   
---
+++ workhours.py
@@ -0,0 +1,74 @@
+import json
+from datetime import datetime
+
+def obtain_workhours_this_month():
+    
+    file_path = get_month_file()
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        
+    return extract_time_at_workplace(data)
+
+def get_month_file():
+    today = datetime.today()
+    formatted_date = f"{today.strftime('%Y_%B').upper()}.json"
+
+    return formatted_date
+
+def extract_time_at_workplace(data):          
+        monthly_time_json_table = {}
+
+        for entry in data['timelineObjects']:
+            if 'placeVisit' in entry:
+                location_data = entry['placeVisit']['location']
+                if 'name' in location_data and (location_data['name'] == "Consid AB" or location_data['name'] == "BorgWarner"):
+                    duration = entry['placeVisit']['duration']
+                    date = extract_date(duration)
+                    if date in monthly_time_json_table:
+                        monthly_time_json_table[date] += obtain_work_in_minutes(duration)
+                    else:
+                        monthly_time_json_table[date] = obtain_work_in_minutes(duration)
+
+        monthly_worktime_json_table = subtract_lunch_time(monthly_time_json_table)
+        monthly_rounded_worktime_json_table = round_workhours(monthly_worktime_json_table)
+
+        return monthly_rounded_worktime_json_table
+
+def subtract_lunch_time(monthly_time_json_table):
+
+    minutes_eating_lunch = 30
+
+    for entry in monthly_time_json_table:
+        monthly_time_json_table[entry] = monthly_time_json_table[entry] - minutes_eating_lunch
+
+    return monthly_time_json_table
+
+def round_workhours(monthly_worktime_json_table):
+    half_hour_in_minutes = 30
+
+    for entry in monthly_worktime_json_table:
+        time_to_add = half_hour_in_minutes - monthly_worktime_json_table[entry] % half_hour_in_minutes
+        monthly_worktime_json_table[entry] = monthly_worktime_json_table[entry] + time_to_add
+
+    return monthly_worktime_json_table
+
+    
+# Date is expressed in yyyy-mm-dd
+def extract_date(duration):
+    startTimestamp = duration['startTimestamp'] # Contains the format: yyyy-mm-ddThh:mm:ssZ
+    return startTimestamp[:10]
+
+def obtain_work_in_minutes(duration):
+
+    seconds_per_minutes = 60
+
+    # Timestamp in following format hh:mm:ss
+    start_timestamp = duration['startTimestamp'][11:19] 
+    end_timestamp = duration['endTimestamp'][11:19]
+
+    start_time_object = datetime.strptime(start_timestamp, "%H:%M:%S")
+    end_time_object = datetime.strptime(end_timestamp, "%H:%M:%S")
+    time_difference = end_time_object - start_time_object
+
+    return round(time_difference.total_seconds() / seconds_per_minutes)
